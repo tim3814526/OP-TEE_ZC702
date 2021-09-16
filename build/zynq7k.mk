10792a7aa52f6289cf23c741c5a5845819d56dd5
tim3814526 <tim71126@gmail.com>
2021-09-16 02:35:33 -0700

    add optee-examples

--- build/zynq7k.mk
+++ build/zynq7k.mk
@@ -1,7 +1,7 @@
 PLATFORM		?= zc702
 BSP_PATH		?= ../xilinx-${PLATFORM}-v$(PETALINUX_VER)-final.bsp
 PRJ_PATH		?= ../petalinux-optee-$(PLATFORM)
-OPTEE_VER		?= latest
+OPTEE_VER		?= 3.11.0
 
 PYTHON_PATH	?= ${PRJ_PATH}/project-spec/meta-user/recipes-devtools/python
 
@@ -65,7 +65,7 @@ create: check
 	-o ${PYTHON_PATH}/python-pycryptodome.inc
 	@#
 	@# Add packages to the image
-	@echo IMAGE_INSTALL_append = \" optee-os optee-client\" >> ${PRJ_PATH}/project-spec/meta-user/conf/petalinuxbsp.conf
+	@echo IMAGE_INSTALL_append = \" optee-os optee-client optee-examples\" >> ${PRJ_PATH}/project-spec/meta-user/conf/petalinuxbsp.conf
 	@#
 	@# Add optee kernel options to the exisiting kernel append recipe
 	@echo SRC_URI_append += \"file://kernel_optee.cfg\" >> ${PRJ_PATH}/project-spec/meta-user/recipes-kernel/linux/linux-xlnx_%.bbappend
---
+++ build/zynq7k/optee/optee-examples_%.bbappend
@@ -0,0 +1,1 @@
+COMPATIBLE_MACHINE_zynq = "zynq-generic|ultra96-zynq"
